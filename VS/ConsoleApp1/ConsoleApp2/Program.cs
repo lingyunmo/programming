11e25453732afb42967f7de4494c991fd32655a7
lingyunmo <2087789639@qq.com>
2022-04-29 22:56:15 +0800

    vs

---
+++ VS/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //成员变量
+        internal double length;
+        internal double width;
+
+        double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("长度： {0}", length);
+            Console.WriteLine("宽度： {0}", width);
+            Console.WriteLine("面积： {0}", GetArea());
+        }
+    }//end class Rectangle    
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
